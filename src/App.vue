224de025056c30f27c8717637516a68fa783510d
Ywoosang <opellong13@gmail.com>
2021-12-03 05:19:00 +0900

    Add default config files

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <nav-bar/>
+    <router-view/>
+  </main>
+</template>
+
+<script>
+import NavBar from './components/common/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
+
+<style>
+* {
+  /* border: 1px solid red; */
+}
+
+html {
+  height: 100%;
+}
+
+body {
+    margin: 0;
+    height: 100%;
+  }
+
+main {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
