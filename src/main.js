eafa10f92b307f24ae81316d56f63dfac597aa47
Ywoosang <opellong13@gmail.com>
2021-12-05 04:36:55 +0900

    Add vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import router from './router';
-import { store } from './store'; 
-// font-awesome
+import { store } from './store';
+// fontawasome 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+library.add(faEnvelope);
+library.add(faLock);
+
+//router
 Vue.use(VueRouter);
-
-// Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
